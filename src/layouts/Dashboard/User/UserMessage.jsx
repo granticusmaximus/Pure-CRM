308d94101dea5d5a311fcca086ba802d8d3ceb10
Grant Watson <grant@triumphdesigns.org>
2018-09-08 19:37:28 -0400

    Update

--- src/layouts/Dashboard/User/UserMessage.jsx
+++ src/layouts/Dashboard/User/UserMessage.jsx
@@ -6,7 +6,6 @@ const Messages = () => (
   <div className="container">
   <Alert color="primary">Please be patient as Grant Watson adds more components</Alert>
     <h1>User Messages</h1>
-    <p>More info coming soon</p>
   </div>
 );
 
