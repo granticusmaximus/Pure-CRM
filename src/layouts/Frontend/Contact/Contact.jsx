5b5f36a24befc2f5ad53f895c4252257971e3fdd
Grant Watson <grant@triumphdesigns.org>
2018-09-10 14:44:49 -0400

    Change to Contact Page

--- src/layouts/Frontend/Contact/Contact.jsx
+++ src/layouts/Frontend/Contact/Contact.jsx
@@ -1,15 +1,93 @@
-import React, { Component } from "react";
 import "../../../assets/css/App.css";
-import { Alert } from "reactstrap";
+import React, { Component } from "react";
+import { FormGroup, FormControl } from "react-bootstrap";
+import { Button, Label, Input } from "reactstrap";
 
 class ContactPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullName: "",
+      email: "",
+      phone: "",
+      message: ""
+    };
+  }
+
+  validateForm() {
+    return (
+      this.state.fullName.length > 0 &&
+      this.state.email.length > 0 &&
+      this.state.phone.length > 0 &&
+      this.state.message.length > 0
+    );
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
   render() {
     return (
-      <div className="App">
-        <div className="container">
-        <Alert color="primary">Please be patient as Grant Watson adds more components</Alert>
-        <h2>Contact Page</h2>
-        <p>I am adding more to this tool, right now, it is in the works to be a CRM Tool</p>
+      <div className="container">
+        <div className="loginBox">
+          <center>
+            <div className="loginHeader">Contact Form</div>
+          </center>
+          <div className="Login">
+            <form onSubmit={this.handleSubmit}>
+              <FormGroup controlId="fullName" bsSize="large">
+                <Label>Full Name</Label>
+                <FormControl
+                  autoFocus
+                  type="fullName"
+                  value={this.state.fullName}
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+              <FormGroup controlId="email" bsSize="large">
+                <Label>Email</Label>
+                <FormControl
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+              <FormGroup controlId="password" bsSize="large">
+                <Label>Telephone Number</Label>
+                <FormControl
+                  value={this.state.phone}
+                  onChange={this.handleChange}
+                  type="password"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="aboutYouText">Leave a Message</Label>
+                <Input
+                  type="textarea"
+                  value={this.state.message}
+                  name="text"
+                  onChange={this.handleChange}
+                  id="aboutYouText"
+                />
+              </FormGroup>
+              <Button
+                color="warning"
+                block
+                bsSize="large"
+                disabled={!this.validateForm()}
+                type="submit"
+              >
+                Register
+              </Button>
+            </form>
+          </div>
         </div>
       </div>
     );
