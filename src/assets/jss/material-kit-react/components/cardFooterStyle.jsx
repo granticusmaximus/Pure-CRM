68858d278aa909c35cfb498a0bb0d02455426c3c
Grant Watson <grant@triumphdesigns.org>
2018-09-10 08:51:41 -0400

    Added Files

---
+++ src/assets/jss/material-kit-react/components/cardFooterStyle.jsx
@@ -0,0 +1,11 @@
+const cardFooterStyle = {
+    cardFooter: {
+      display: "flex",
+      alignItems: "center",
+      backgroundColor: "transparent",
+      padding: "0.9375rem 1.875rem"
+    }
+  };
+  
+  export default cardFooterStyle;
+  
