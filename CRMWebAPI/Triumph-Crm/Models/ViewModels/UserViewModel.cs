4909b7fd9a79eaa8b7504fa5c37d343a01def331
Grant Watson <grant@triumphdesigns.org>
2018-09-12 07:57:27 -0400

    Updated C# Models and ViewModels
    Added the following:
    Project | ProjectViewModel
    UserViewModel
    ClientViewModel

---
+++ CRMWebAPI/Triumph-Crm/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TriumphCrm.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "First Name:")]
+        public string FName { get; set; }
+
+        [Display(Name = "Last Name:")]
+        public string LName { get; set; }
+    }
+}
