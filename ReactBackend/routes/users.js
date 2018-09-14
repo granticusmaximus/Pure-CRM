656f34c5b52d73f4532e7c605b5d4937ca635fcc
Grant Watson <grant@triumphdesigns.org>
2018-09-14 10:41:33 -0400

    Rearranging file structure
    Will post an update to the ReadMe.md file once I have an accurate display

--- ReactBackend/routes/users.js
+++ ReactBackend/routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  // Comment out this line:
+  //res.send('respond with a resource');
+
+  // And insert something like this instead:
+  res.json([
+    { id: 1, username: "grantAdmin" },
+    { id: 2, username: "grant_watson" },
+    { id: 3, username: "krissy_watson" },
+    { id: 4, username: "ozzy_the_creator" },
+    { id: 5, username: "frankOceania" }
+  ]);
 });
 
 module.exports = router;
