c2abc89928ca79541cbaefa911fdeca1169b695b
Grant Watson <grant@triumphdesigns.org>
2018-09-14 12:01:23 -0400

    Starting Express work

--- ReactBackend/routes/users.js
+++ ReactBackend/routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  // Comment out this line:
+  //res.send('respond with a resource');
+
+  // The following is test data:
+  res.json([
+    { id: 1, username: "grantAdmin" },
+    { id: 2, username: "grant_watson" },
+    { id: 3, username: "ozzy_nova" },
+    { id: 4, username: "orykia" }
+  ]);
 });
 
 module.exports = router;
